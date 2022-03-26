86c685b368575511888162d916263a5c75f184e9
Bhushan Borole <borolebhushan8@gmail.com>
2022-03-26 15:08:28 +0000

    Combined bfs and dfs into a single function

--- bfs-dfs/solve.py
+++ bfs-dfs/solve.py
@@ -9,7 +9,6 @@ class TestCases:
         'goal': (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23)
     }
 
-
 def trace_path(visited, source, goal):
     path = []
     temp = goal
@@ -18,8 +17,7 @@ def trace_path(visited, source, goal):
         path.append(rot)
     return path[::-1]
 
-
-def bfs(source, goal):
+def search(source, goal, func='DFS'):
     cube = Cube()
 
     visited = {source: (source, -1)}
@@ -28,7 +26,10 @@ def bfs(source, goal):
     nodes.append(source)
 
     while nodes:
-        node = nodes.popleft()
+        if func == 'BFS':
+            node = nodes.popleft()
+        else:
+            node = nodes.pop()
         if node == goal:
             print('Goal State Reached')
             return visited
@@ -41,45 +42,20 @@ def bfs(source, goal):
                 else:
                     continue
 
-
-def dfs(source, goal):
-    cube = Cube()
-
-    visited = {source: (source, -1)}
-    nodes = deque()
-    nodes.append(source)
-    # nodes.insert(0, source)
-    while nodes:
-        # node = nodes.popleft()
-        node = nodes.pop()
-        if node == goal:
-            print('Goal State Reached/dfs')
-            return visited
-        else:
-            for rot in cube.rotations:
-                temp_node = cube.apply_rotation(node, cube.rotations[rot])
-                if not temp_node in visited:
-                    visited[temp_node] = (node, rot)
-                    nodes.append(temp_node)
-                    # nodes.insert(0, temp_node)
-                else:
-                    continue
-
-
 def solve(test_case, func):
     source = test_case['source']
     goal = test_case['goal']
     start = time.time()
-    visited = func(source, goal)
+    visited = search(source, goal, func)
     end = time.time()
     path = trace_path(visited, source, goal)
-    print(f'The time taken to reach the goal state using {func.__name__}: {round(end - start, 2)}')
-    # print(f'The path is: {path}')
-
+    print(f'The time taken to reach the goal state using {func}: {round(end - start, 2)}')
+    if func == 'BFS':
+        print(f'The path is: {path}')
 
 def main():
-    solve(TestCases.test_case1, bfs)
-    solve(TestCases.test_case1, dfs)
+    solve(TestCases.test_case1, 'BFS')
+    solve(TestCases.test_case1, 'DFS')
 
 
 if __name__ == '__main__':
