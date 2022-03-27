d58f66184a92ec9232c1ecc1d206a89d11aca5ed
Yatheendra Pravan <pravan.yatheen@gmail.com>
2022-03-27 12:03:07 +0100

    Initial Configuration

---
+++ 3x3/cube.py
@@ -0,0 +1,150 @@
+import numpy as np
+class RubicsCube:
+    def __init__(self) -> None:
+        self.solved_config = np.array([
+            ['W', 'W', 'W'],
+            ['W', 'W', 'W'],
+            ['W', 'W', 'W'],
+            ['G', 'G', 'G'],
+            ['G', 'G', 'G'],
+            ['G', 'G', 'G'],
+            ['Y', 'Y', 'Y'],
+            ['Y', 'Y', 'Y'],
+            ['Y', 'Y', 'Y'],
+            ['O', 'O', 'O'],
+            ['O', 'O', 'O'],
+            ['O', 'O', 'O'],
+            ['R', 'R', 'R'],
+            ['R', 'R', 'R'],
+            ['R', 'R', 'R'],
+            ['B', 'B', 'B'],
+            ['B', 'B', 'B'],
+            ['B', 'B', 'B'],
+        ])
+        self.cube = np.copy(self.solved_config)
+
+    def is_cube_solved(self):
+       return np.array_equal(self.cube,self.solved_config) 
+    
+    def PrintCube(self):
+        print("             ", self.cube[0, 0:3])
+        print("             ", self.cube[1, 0:3])
+        print("             ", self.cube[2, 0:3])
+        print(self.cube[3, 0:3], self.cube[6, 0:3], self.cube[9, 0:3], self.cube[12, 0:3])
+        print(self.cube[4, 0:3], self.cube[7, 0:3], self.cube[10, 0:3], self.cube[13, 0:3])
+        print(self.cube[5, 0:3], self.cube[8, 0:3], self.cube[11, 0:3], self.cube[14, 0:3])
+        print("             ", self.cube[15, 0:3])
+        print("             ", self.cube[16, 0:3])
+        print("             ", self.cube[17, 0:3])
+        print("="*25)
+
+    def scramble(self,action_sequence):
+        cubeSolver = CubeSolver()
+        for action in action_sequence:
+            action(cubeSolver,self.cube)
+
+
+class CubeSolver:
+
+    def front_clockwise(self,cube):
+        #print("[INFO] Rotating front cube face in clockwise direction")
+        cube[6:9, 0:3] = np.fliplr(cube[6:9, 0:3].transpose())
+        temp1 = np.array(cube[2, 0:3])
+        temp2 = np.array(cube[9:12, 0])
+        temp3 = np.array(cube[15, 0:3])
+        temp4 = np.array(cube[3:6, 2])
+        cube[2, 0:3] = np.fliplr([temp4])[0]
+        cube[9:12, 0] = temp1
+        cube[15, 0:3] = np.fliplr([temp2])[0]
+        cube[3:6, 2] = temp3
+    
+    def front_anti_clockwise(self,cube):
+        #print("[INFO] Rotating front face in anti-clockwise direction")
+        self.front_clockwise(cube)
+        self.front_clockwise(cube)
+        self.front_clockwise(cube)
+
+    def up_clockwise(self,cube):  # action 3
+        cube[0:3, 0:3] = np.fliplr(cube[0:3, 0:3].transpose())
+        temp1 = np.array(cube[12, 0:3])
+        temp2 = np.array(cube[9, 0:3])
+        temp3 = np.array(cube[6, 0:3])
+        temp4 = np.array(cube[3, 0:3])
+        cube[12, 0:3] = temp4
+        cube[9, 0:3] = temp1
+        cube[6, 0:3] = temp2
+        cube[3, 0:3] = temp3
+
+
+    def up_anti_clockwise(self,cube):  # acion 4
+        self.up_clockwise(cube)
+        self.up_clockwise(cube)
+        self.up_clockwise(cube)
+
+    def down_clockwise(self,cube):  # action 5 Front down clock wise
+        cube[15:18, 0:3] = np.fliplr(cube[15:18, 0:3].transpose())
+        temp1 = np.array(cube[8, 0:3])
+        temp2 = np.array(cube[11, 0:3])
+        temp3 = np.array(cube[14, 0:3])
+        temp4 = np.array(cube[5, 0:3])
+        cube[8, 0:3] = temp4
+        cube[11, 0:3] = temp1
+        cube[14, 0:3] = temp2
+        cube[5, 0:3] = temp3
+
+    def down_anti_clockwise(self,cube):  # action 6
+        self.down_clockwise(cube)
+        self.down_clockwise(cube)
+        self.down_clockwise(cube)
+
+    def left_clockwise(self,cube): # action 7
+        cube[3:6, 0:3] = np.fliplr(cube[3:6, 0:3].transpose())
+        temp1 = np.array(cube[0:3, 0])
+        temp2 = np.array(cube[6:9, 0])
+        temp3 = np.array(cube[15:18, 0])
+        temp4 = np.array(cube[12:15, 2])
+        cube[0:3, 0] = np.fliplr([temp4])[0]
+        cube[6:9, 0] = temp1
+        cube[15:18, 0] = temp2
+        cube[12:15, 2] = np.fliplr([temp3])[0]
+
+
+    def left_anti_clockwise(self,cube):  # action 8
+        self.left_clockwise(cube)
+        self.left_clockwise(cube)
+        self.left_clockwise(cube)
+
+
+    def right_clockwise(self,cube):  # action 9 Front right clock wise
+        cube[9:12, 0:3] = np.fliplr(cube[9:12, 0:3].transpose())
+        temp1 = np.array(cube[0:3, 2])
+        temp2 = np.array(cube[12:15, 0])
+        temp3 = np.array(cube[15:18, 2])
+        temp4 = np.array(cube[6:9, 2])
+        cube[0:3, 2] = temp4
+        cube[12:15, 0] = np.fliplr([temp1])[0]
+        cube[15:18, 2] = np.fliplr([temp2])[0]
+        cube[6:9, 2] = temp3
+
+
+    def right_anti_clockwise(self,cube):  # action 10
+        self.right_clockwise(cube)
+        self.right_clockwise(cube)
+        self.right_clockwise(cube)
+
+
+    def back_clockwise(self,cube):  # action 11 Front  back clock wise
+        cube[12:15, :] = np.fliplr(cube[12:15, :].transpose())
+        temp1 = np.array(cube[0, 0:3])
+        temp2 = np.array(cube[3:6, 0])
+        temp3 = np.array(cube[17, 0:3])
+        temp4 = np.array(cube[9:12, 2])
+        cube[0, 0:3] = temp4
+        cube[3:6, 0] = np.fliplr([temp1])[0]
+        cube[17, 0:3] = temp2
+        cube[9:12, 2] = np.fliplr([temp3])[0]
+
+    def back_anti_clockwise(self,cube):  # action 12
+        self.back_clockwise(cube)
+        self.back_clockwise(cube)
+        self.back_clockwise(cube)
---
+++ 3x3/scrambler.py
@@ -0,0 +1,63 @@
+import numpy as np
+from cube import CubeSolver
+
+class Scrammbler:
+    def __init__(self):
+        self.action_map = {
+            "F" : getattr(CubeSolver,"front_clockwise"),
+            "L" : getattr(CubeSolver,"left_clockwise"),
+            "R" : getattr(CubeSolver,"right_clockwise"),
+            "D" : getattr(CubeSolver,"down_clockwise"),
+            "U" : getattr(CubeSolver,"up_clockwise"),
+            "B" : getattr(CubeSolver,"back_clockwise"),
+            "F'" : getattr(CubeSolver,"front_anti_clockwise"),
+            "L'" : getattr(CubeSolver,"left_anti_clockwise"),
+            "R'" : getattr(CubeSolver,"right_anti_clockwise"),
+            "D'" : getattr(CubeSolver,"down_anti_clockwise"),
+            "U'" : getattr(CubeSolver,"up_anti_clockwise"),
+            "B'" : getattr(CubeSolver,"back_anti_clockwise"),
+        }
+        self.negate_action = {
+            "F" : "F'",
+            "L" : "L'",
+            "R" : "R'",
+            "D" : "D'",
+            "U" : "U'",
+            "B" : "B'",
+            "F'" : "F",
+            "L'" : "L",
+            "R'" : "R",
+            "D'" : "D",
+            "U'" : "U",
+            "B'" : "B",
+        }
+
+    def get_scramble_action_sequence(self,scramble_string):
+        scramble_sequence = []
+        actions = scramble_string.split(" ")
+        
+        for action in actions:
+            if action not in self.action_map:
+                scramble_sequence.append(self.action_map[action[:-1]])
+                scramble_sequence.append(self.action_map[action[:-1]])
+            else:
+                scramble_sequence.append(self.action_map[action])
+        return scramble_sequence
+    
+    def get_unscramble_action_sequence(self,scramble_string):
+        unscramble_sequence = []
+        actions = reversed(scramble_string.split(" "))
+        for action in actions:
+            if action not in self.action_map:
+                negateAction = self.negate_action[action[:-1]]
+                unscramble_sequence.append(self.action_map[negateAction])
+                unscramble_sequence.append(self.action_map[negateAction])
+            else:
+                negateAction = self.negate_action[action]
+                unscramble_sequence.append(self.action_map[negateAction])
+        return unscramble_sequence
+
+    def scramble(self,scramble_string):
+        scramble_sequence = self.get_scramble_action_sequence(scramble_string)
+        unscramble_sequence = self.get_unscramble_action_sequence(scramble_string)
+        return scramble_sequence,unscramble_sequence
