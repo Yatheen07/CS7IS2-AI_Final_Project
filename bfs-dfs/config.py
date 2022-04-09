0ea51a2fff6e607be41112d7e68f4fedf57ca09d
Bhushan Borole <borolebhushan8@gmail.com>
2022-04-09 13:14:29 +0100

    generic config generator

--- bfs-dfs/config.py
+++ bfs-dfs/config.py
@@ -12,5 +12,5 @@ FACES_LIST = [TOP, LEFT, FRONT, RIGHT, BACK, BOTTOM]
 def get_solved_config(size):
     config = []
     for face in FACES_LIST:
-        config.extend([[face]*size] * size))
+        config.extend([[face]*size] * size)
     return np.array(config)
