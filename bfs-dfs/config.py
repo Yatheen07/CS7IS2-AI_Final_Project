1eafc3e4606527ff6f75faa3da130f9341bcbbc2
Bhushan Borole <borolebhushan8@gmail.com>
2022-04-09 13:14:12 +0100

    generic config generator

--- bfs-dfs/config.py
+++ bfs-dfs/config.py
@@ -1,40 +1,16 @@
 import numpy as np
 
-config_2x2 = np.array([
-    ['W', 'W'],
-    ['W', 'W'],
-    ['G', 'G'],
-    ['G', 'G'],
-    ['R', 'R'],
-    ['R', 'R'],
-    ['B', 'B'],
-    ['B', 'B'],
-    ['O', 'O'],
-    ['O', 'O'],
-    ['Y', 'Y'],
-    ['Y', 'Y'],
-])
 
-config_3x3 = np.array([
-    ['W', 'W', 'W'],
-    ['W', 'W', 'W'],
-    ['W', 'W', 'W'],
-    ['G', 'G', 'G'],
-    ['G', 'G', 'G'],
-    ['G', 'G', 'G'],
-    ['R', 'R', 'R'],
-    ['R', 'R', 'R'],
-    ['R', 'R', 'R'],
-    ['B', 'B', 'B'],
-    ['B', 'B', 'B'],
-    ['B', 'B', 'B'],
-    ['O', 'O', 'O'],
-    ['O', 'O', 'O'],
-    ['O', 'O', 'O'],
-    ['Y', 'Y', 'Y'],
-    ['Y', 'Y', 'Y'],
-    ['Y', 'Y', 'Y']
-])
+TOP = 'W'
+LEFT = 'G'
+FRONT = 'R'
+RIGHT = 'B'
+BACK = 'O'
+BOTTOM = 'Y'
+FACES_LIST = [TOP, LEFT, FRONT, RIGHT, BACK, BOTTOM]
 
 def get_solved_config(size):
-    return config_2x2 if size == 2 else config_3x3
+    config = []
+    for face in FACES_LIST:
+        config.extend([[face]*size] * size))
+    return np.array(config)
