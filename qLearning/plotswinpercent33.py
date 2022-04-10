cc46c16fa33b333594830ed5edc9f2f0042f4747
dee-vy <48962981+dee-vy@users.noreply.github.com>
2022-04-10 23:47:16 +0100

    Width and opacity updated

--- qLearning/plotswinpercent33.py
+++ qLearning/plotswinpercent33.py
@@ -20,10 +20,10 @@ qlearning = [None, 100,
 manual_baseline = [None, 100, 100, 100, 100, 100, 100, 100, 100]
 scrabmle_length = [None, 1, 2, 3, 4, 5, 6, 7, 15]
 
-plt.plot(scrabmle_length, bfs, label="BFS")
-plt.plot(scrabmle_length, astar, label="A*")
-plt.plot(scrabmle_length, qlearning, label="qLearning")
-plt.plot(scrabmle_length, manual_baseline, label="Baseline")
+plt.plot(scrabmle_length, bfs, label="BFS", lw=3)
+plt.plot(scrabmle_length, astar, label="A*", alpha=0.3, lw=7)
+plt.plot(scrabmle_length, qlearning, label="qLearning", lw=0.8)
+plt.plot(scrabmle_length, manual_baseline, label="Baseline", lw=0.4)
 plt.xlabel("Scramble Length")
 plt.ylabel("Win percentage")
 # plt.yscale("log")
