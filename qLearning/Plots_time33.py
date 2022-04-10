a6a77fae5d038eb1ba4e0432e1eb0268403f481f
dee-vy <48962981+dee-vy@users.noreply.github.com>
2022-04-10 23:22:36 +0100

    added final values

--- qLearning/Plots_time33.py
+++ qLearning/Plots_time33.py
@@ -2,26 +2,28 @@
 import matplotlib.pyplot as plt
 
 bfs = [None, 0.001,
-       0.008,
-       0.177,
-       0.705,
-       4.37,
-       39.4,
-       327, None]
+       0.01,
+       0.266,
+       1,
+       7,
+       75,
+       1937, None]
+
 astar = [None, 0.008,
          0.01,
          0.0204,
          0.0197,
          0.023,
          0.03,
-         327, None]
-qlearning = [None, 1.14,
-             1.33,
-             1.42,
-             1.99,
-             1.78,
-             1.88,
-             1.93, 2.04]
+         1937, None]
+
+qlearning = [None, 5.45,
+             5.62,
+             5.65,
+             5.6,
+             5.62,
+             5.64,
+             5.7, 5.83]
 
 manual_baseline = [None, 1, 2, 10, 30, 180, 192, 210, 211]
 scrabmle_length = [None, 1, 2, 3, 4, 5, 6, 7, 15]
@@ -33,6 +35,6 @@ plt.plot(scrabmle_length, manual_baseline, label="Baseline")
 plt.xlabel("Scramble Length")
 plt.ylabel("Time (s) - logarithmic scale")
 plt.yscale("log")
-plt.title("Time Comparison")
+plt.title("Time Comparison for 3x3x3 Cube")
 plt.legend()
 plt.show()
--- qLearning/plotswinpercent33.py
+++ qLearning/plotswinpercent33.py
@@ -1,29 +1,23 @@
 # 3x3 Win percentage
 import matplotlib.pyplot as plt
 
-bfs = [None, 0.001,
-       0.008,
-       0.177,
-       0.705,
-       4.37,
-       39.4,
-       327, None]
-astar = [None, 0.008,
-         0.01,
-         0.0204,
-         0.0197,
-         0.023,
-         0.03,
-         327, None]
-qlearning = [None, 1.14,
-             1.33,
-             1.42,
-             1.99,
-             1.78,
-             1.88,
-             1.93, 2.04]
+bfs = [None, 100, 100, 100, 100, 100, 100, 100, None]
+astar = [None, 100,
+         100,
+         100,
+         100,
+         100,
+         90,
+         90, None]
+qlearning = [None, 100,
+             100,
+             100,
+             90,
+             80,
+             70,
+             50, 5.83]
 
-manual_baseline = [None, 1, 2, 10, 30, 180, 192, 210, 211]
+manual_baseline = [None, 100, 100, 100, 100, 100, 100, 100, 100]
 scrabmle_length = [None, 1, 2, 3, 4, 5, 6, 7, 15]
 
 plt.plot(scrabmle_length, bfs, label="BFS")
@@ -31,8 +25,8 @@ plt.plot(scrabmle_length, astar, label="A*")
 plt.plot(scrabmle_length, qlearning, label="qLearning")
 plt.plot(scrabmle_length, manual_baseline, label="Baseline")
 plt.xlabel("Scramble Length")
-plt.ylabel("Time (s) - logarithmic scale")
-plt.yscale("log")
-plt.title("Time Comparison")
+plt.ylabel("Win percentage")
+# plt.yscale("log")
+plt.title("Winning Percentages for 3x3x3 Cube")
 plt.legend()
 plt.show()
