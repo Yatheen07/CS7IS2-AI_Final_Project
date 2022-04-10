e441d6417340616cb0397763a2092a2152b0533f
Bhushan Borole <borolebhushan8@gmail.com>
2022-04-10 22:31:32 +0100

    Show game summary

--- search/search.py
+++ search/search.py
@@ -5,30 +5,33 @@ from model.cube import RubicsCube
 import numpy as np
 from model.scrambler import Scrammbler
 import util
-class Search:
 
+
+class Search:
     def __init__(self, rubicsCube, depth=None) -> None:
         self.rubicsCube = rubicsCube
         self.cube = self.rubicsCube.cube
         self.size = self.rubicsCube.size
         self.depth = depth
         self.rotations = Scrammbler().action_map.items()
-        #self.rotations = Scrammbler().rotations.items()
+        # self.rotations = Scrammbler().rotations.items()
         self.start_state_config = "START_STATE"
-    
+
     def _trace_path(self, node, visited):
         path = []
-        current_config,action = visited[node.get_configuration_string()]
+        current_config, action = visited[node.get_configuration_string()]
         path.append(action)
         while current_config != self.start_state_config:
             current_config, action = visited[current_config]
             path.append(action)
         return path[::-1][1:]
 
-    def traverse_states(self, frontier,pop_action):
+    def traverse_states(self, frontier, pop_action):
         goal_reached = False
         number_of_states = 0
-        visited = {self.rubicsCube.get_configuration_string(): (self.start_state_config,"-1")}
+        visited = {
+            self.rubicsCube.get_configuration_string(): (self.start_state_config, "-1")
+        }
         currentNode = copy.deepcopy(self.rubicsCube)
         frontier.append(currentNode)
         while frontier:
@@ -39,7 +42,7 @@ class Search:
                 goal_reached = True
                 break
             else:
-                for action,rotation in self.rotations:
+                for action, rotation in self.rotations:
                     temp_cube = RubicsCube(self.size)
                     new_node = copy.deepcopy(currentNode)
                     new_node.scramble([rotation])
@@ -48,44 +51,78 @@ class Search:
                     config_string = temp_cube.get_configuration_string()
 
                     if not config_string in visited:
-                        visited[config_string] = (currentNode.get_configuration_string(), action)
+                        visited[config_string] = (
+                            currentNode.get_configuration_string(),
+                            action,
+                        )
                         frontier.append(temp_cube)
-                    
+
                     if temp_cube.is_cube_solved():
                         print(temp_cube)
                         goal_reached = True
-                        return goal_reached,visited,temp_cube,len(visited)-1
+                        return goal_reached, visited, temp_cube, len(visited) - 1
 
             if self.depth:
                 self.depth -= 1
-        return goal_reached,visited,currentNode,len(visited)-1
+        return goal_reached, visited, currentNode, len(visited) - 1
 
-    def printResults(self,path,goal_reached,algorithm,time_elapsed,number_of_states):
+    def printResults(
+        self, path, goal_reached, algorithm, time_elapsed, number_of_states
+    ):
         print(f"Unscrambling Rubics Cube using {algorithm}")
-        print("--"*25)
+        print("--" * 25)
         print(f"[INFO] Goal-Reached? {goal_reached}")
         if goal_reached:
             print(f"[INFO] Path: {path}")
         print(f"[INFO] Elapsed time: {time_elapsed}")
         print(f"[INFO] Number of States: {number_of_states}")
-        print("--"*25)
+        print("--" * 25)
 
     def bfs(self):
         start = time.time()
         frontier = deque()
-        pop_action = getattr(deque,"popleft")
-        goal_reached, visited, currentNode,number_of_states = self.traverse_states(frontier,pop_action)
-        self.printResults(path=self._trace_path(currentNode,visited),goal_reached=goal_reached,algorithm="Breadth First Search",time_elapsed=time.time() - start,number_of_states=number_of_states)
+        pop_action = getattr(deque, "popleft")
+        goal_reached, visited, currentNode, number_of_states = self.traverse_states(
+            frontier, pop_action
+        )
+        # self.printResults(
+        #     path=self._trace_path(currentNode, visited),
+        #     goal_reached=goal_reached,
+        #     algorithm="Breadth First Search",
+        #     time_elapsed=time.time() - start,
+        #     number_of_states=number_of_states,
+        # )
+        return (
+            goal_reached,
+            self._trace_path(currentNode, visited),
+            time.time() - start,
+            number_of_states,
+        )
 
     def dfs(self):
         start = time.time()
         frontier = list()
-        pop_action = getattr(list,"pop")
-        goal_reached, visited, currentNode,number_of_states = self.traverse_states(frontier,pop_action)
-        self.printResults(path=self._trace_path(currentNode,visited),goal_reached=goal_reached,algorithm="Depth First Search",time_elapsed=time.time() - start,number_of_states=number_of_states)
-    
+        pop_action = getattr(list, "pop")
+        goal_reached, visited, currentNode, number_of_states = self.traverse_states(
+            frontier, pop_action
+        )
+        # self.printResults(
+        #     path=self._trace_path(currentNode, visited),
+        #     goal_reached=goal_reached,
+        #     algorithm="Depth First Search",
+        #     time_elapsed=time.time() - start,
+        #     number_of_states=number_of_states,
+        # )
+        return (
+            goal_reached,
+            self._trace_path(currentNode, visited),
+            time.time() - start,
+            number_of_states,
+        )
+
     def iddfs(self):
         start = time.time()
+
         class CubeState:
             state = None
             parent = None
@@ -93,24 +130,29 @@ class Search:
             cost = 0
 
         goal_reached = False
-        visited = {self.cube.get_configuration_string(): (self.start_state_config,"-1")}
-        
+        visited = {
+            self.cube.get_configuration_string(): (self.start_state_config, "-1")
+        }
+
         startState = CubeState()
         startState.state = self.cube
-        
+
         cost_limit = 1
         nodes = 0
         frontier = list()
         branching_factors = list()
         currentState = copy.deepcopy(startState)
-        while cost_limit<=self.depth:
+        while cost_limit <= self.depth:
             frontier.append(startState)
             while len(frontier) != 0:
                 currentState = frontier.pop()
-                
+
                 if currentState.state.is_cube_solved():
-                    print('[INFO] Goal Height:', currentState.cost)
-                    print('[INFO] Branching Factor:', sum(branching_factors)/len(branching_factors))
+                    print("[INFO] Goal Height:", currentState.cost)
+                    print(
+                        "[INFO] Branching Factor:",
+                        sum(branching_factors) / len(branching_factors),
+                    )
                     print("[INFO] Nodes Generated:", nodes)
                     print(f"[INFO] Elapsed time: {time.time() - start}")
                     goal_reached = True
@@ -121,7 +163,7 @@ class Search:
                     child_cost = currentState.cost + 1
                     branch = 0
 
-                    for key,action in self.rotations:
+                    for key, action in self.rotations:
                         nodes = nodes + 1
                         new = CubeState()
                         new.state = copy.deepcopy(currentState.state)
@@ -129,7 +171,7 @@ class Search:
                         new.parent = currentState
                         new.move = key
                         new.state.scramble([action])
-                        
+
                         config_string = new.state.get_configuration_string()
                         frontier.append(new)
                         # if not config_string in visited:
@@ -139,7 +181,10 @@ class Search:
                         # else:
                         #     continue
                     # if curr.parent is not None and np.array_equal(curr.parent.cube, new.cube):
-                    if currentState.parent is not None and (self.commonParent(new.cube, currentState) or self.containsChild(new.cube, frontier)):
+                    if currentState.parent is not None and (
+                        self.commonParent(new.cube, currentState)
+                        or self.containsChild(new.cube, frontier)
+                    ):
                         continue
                     frontier.append(new)
                     branch += 1
@@ -148,48 +193,53 @@ class Search:
             branching_factors.clear()
             cost_limit = cost_limit + 1
 
-        #self.printResults(path=self._trace_path(currentState.state,visited),goal_reached=goal_reached,algorithm="IDDFS",time_elapsed=time.time() - start)
+        # self.printResults(path=self._trace_path(currentState.state,visited),goal_reached=goal_reached,algorithm="IDDFS",time_elapsed=time.time() - start)
 
     # checks if child ascendant of parent
     def commonParent(child, parent):
         curr = parent.parent
         while curr is not None:
-            if np.array_equal(curr.cube, child): return True
+            if np.array_equal(curr.cube, child):
+                return True
             curr = curr.parent
 
         return False
 
-
     # checks if frontier contains child
     def containsChild(child, frontier):
         for curr in frontier:
-            if np.array_equal(curr.cube, child): return True
+            if np.array_equal(curr.cube, child):
+                return True
         return False
 
     def astar(self):
-        print('Solving cube using A*')
+        print("Solving cube using A*")
+
         class CubeState:
             state = None
             g = 0
             h = 0
             parent = None
             move = None
-        bound  = util.patternDatabaseHeuristic(self.rubicsCube)
-        print('Initial Heuristic = ',str(bound))
+
+        bound = util.patternDatabaseHeuristic(self.rubicsCube)
+        print("Initial Heuristic = ", str(bound))
         queue = util.PriorityQueue()
         currentNode = copy.deepcopy(self.rubicsCube)
         start = CubeState()
         start.state = currentNode
         start.h = bound
-        visited = {self.rubicsCube.get_configuration_string(): (self.start_state_config,"-1")}
-        queue.push(start,bound)
-        while(not queue.isEmpty()):
+        visited = {
+            self.rubicsCube.get_configuration_string(): (self.start_state_config, "-1")
+        }
+        queue.push(start, bound)
+        while not queue.isEmpty():
             currentNode = queue.pop()
             if currentNode.state.is_cube_solved():
                 goal_reached = True
                 break
             else:
-                for action,rotation in self.rotations:
+                for action, rotation in self.rotations:
                     new = CubeState()
                     new.state = copy.deepcopy(currentNode.state)
                     new.g = currentNode.g + 1
@@ -199,16 +249,18 @@ class Search:
                     new.h = util.patternDatabaseHeuristic(new.state)
                     config_string = new.state.get_configuration_string()
                     if not config_string in visited:
-                        visited[config_string] = (currentNode.state.get_configuration_string(), action)
-                        queue.push(new,new.g+new.h)
+                        visited[config_string] = (
+                            currentNode.state.get_configuration_string(),
+                            action,
+                        )
+                        queue.push(new, new.g + new.h)
                     if new.state.is_cube_solved():
-                        print('Printing new')
+                        print("Printing new")
                         print(new.state)
                         goal_reached = True
-                        return goal_reached,visited,new.state,len(visited)-1
-        return goal_reached,visited,currentNode,len(visited)-1 
+                        return goal_reached, visited, new.state, len(visited) - 1
+        return goal_reached, visited, currentNode, len(visited) - 1
 
-    
     def astarsearch(self):
         if self.size == 3:
             util.load_cornerpatterns()
@@ -216,7 +268,11 @@ class Search:
         else:
             util.load2dpatterns()
         start = time.time()
-        goal_reached, visited, currentNode,number_of_states = self.astar()
-        self.printResults(path=self._trace_path(currentNode,visited),goal_reached=goal_reached,algorithm="A* Search",time_elapsed=time.time() - start,number_of_states=number_of_states)    
-
-    
+        goal_reached, visited, currentNode, number_of_states = self.astar()
+        return (
+            goal_reached,
+            self._trace_path(currentNode, visited),
+            time.time() - start,
+            number_of_states,
+        )
+        # self.printResults(path=self._trace_path(currentNode,visited),goal_reached=goal_reached,algorithm="A* Search",time_elapsed=time.time() - start,number_of_states=number_of_states)
--- search/solver.py
+++ search/solver.py
@@ -1,42 +1,51 @@
-from model.cube import RubicsCube,Rotator
+from model.cube import RubicsCube, Rotator
 from model.scrambler import Scrammbler
-from model.scramble_configurations import scramble_configurations
+from model.scramble_configurations import get_scramble_config
 import numpy as np
 import time
 import copy
 from search import Search
 
+
 def solve_cube(scramble_size):
-    #Initialise the Cube State - intial state is solved.
-    cube = RubicsCube(size=3)
-    #cubeSolver = CubeSolver()
-    print(cube)
+    # Initialise the Cube State - intial state is solved.
+    wins = 0
+    game_summary = []
+    for i in range(10):
+        game = {}
+        cube = RubicsCube(size=3)
 
-    #Scramble the state based on the preset configurations
-    scrammbler = Scrammbler()
-    scramble_sequence,unscramble_sequence = scrammbler.scramble(scramble_configurations[scramble_size])
+        # Scramble the state based on the preset configurations
+        scrammbler = Scrammbler()
+        scramble_sequence, unscramble_sequence = scrammbler.scramble(
+            get_scramble_config(scramble_size)
+        )
 
-    #Scramble a fresh cube to get a initial state
-    cube.scramble(action_sequence=scramble_sequence)
-    print(cube)
+        # Scramble a fresh cube to get a initial state
+        cube.scramble(action_sequence=scramble_sequence)
 
-    # #Test
-    # cube.scramble(action_sequence=unscramble_sequence)
-    # print(cube)
+        searcher = Search(cube)
+        goal_reached, path, time_elapsed, number_of_states = searcher.bfs()
+        # goal_reached, path, time_elapsed, number_of_states = searcher.dfs()
+        # goal_reached, path, time_elapsed, number_of_states = searcher.astarsearch()
+        if goal_reached:
+            wins += 1
+        game["name"] = f"Game {i+1}"
+        game["result"] = "Won" if goal_reached else "Lost"
+        game["elapsed_time"] = time_elapsed
+        game["Number of States Explored"] = number_of_states
+        game_summary.append(game)
+        print("=" * 75)
 
-    # """Further Steps:
-    #     1. Take the scrambled state of the cube as intial state
-    #     2. Find the steps to solve the cube
-    #     3. Ideally, the result from step 2 should be unscrambled sequence
-    # """
+    for game in game_summary:
+        print(
+            f"[INFO] {game['name']} completed in {game['elapsed_time']}, Result: {game['result']}"
+        )
+    print(f"Win Percentage: {(wins/len(game_summary))*100}")
+    print("=" * 75)
 
-    searcher = Search(cube)
-    #searcher.bfs()
-    #searcher.dfs()
-    # searcher.iddfs()
-    searcher.astarsearch()
 
-for size in range(7):
+for size in range(1, 6):
     print(f"Generating Scrambling sequence of size: {size+1}")
     solve_cube(size)
-    print("="*50)
+    print("=" * 50)
